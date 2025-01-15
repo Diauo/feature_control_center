555a20689e53b48285d5ed81d07bf342575e2bbc
Diauo <2684674727@qq.com>
2025-01-15 08:30:02 +0800

    feat: 隐藏侧边栏滚动条，添加一个ico

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template, send_file
+import os
 
 index_bp = Blueprint('index', __name__)
 
@@ -7,7 +8,13 @@ def index():
     return render_template("index.html")
 
 
+@index_bp.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return send_file("favicon.ico")
+
 @index_bp.before_request
 def joke():
     user_agent = request.headers.get('User-Agent')
     print(f"{user_agent}用户，欢迎来到首页")
+    current_directory = os.getcwd()
+    print(f'当前工作目录: {current_directory}')
